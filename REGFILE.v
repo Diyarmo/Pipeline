0066858b961f3868c5932d26e3cc71cc76cc65e6
Diyar Mohammadi <diyar.mohammadi@ut.ac.ir>
2019-05-25 16:39:47 +0430

    edited regfie module

--- REGFILE.v
+++ REGFILE.v
@@ -1,12 +1,18 @@
 `timescale 1ns/1ns
 
-module REGFILE (input [4:0] ReadReg1, ReadReg2, WriteReg, input [31:0] writeData, input regWrite,
+module REGFILE (input clk, rst, input [4:0] ReadReg1, ReadReg2, WriteReg, input [31:0] writeData, input regWrite,
                 output reg [31:0] readData1, readData2);
   reg [31:0] mem [31:0];
   initial begin
     mem = 1024'b0;
   end
-  always
-
+  always @(negedge clk) begin
+    readData1 <= mem[ReadReg1];
+    readData2 <= mem[ReadReg2];
+  end
+  always @(posedge clk, posedge rst)
+    if (rst)
+      mem = 1024'b0;
+    else if (regWrite) mem[WriteReg] <= writeData;
 
 endmodule
