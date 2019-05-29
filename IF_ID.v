2fb01e2cadf7f755c5c3ec5bed3f84ea8e3eeba0
Diyar Mohammadi <diyar.mohammadi@ut.ac.ir>
2019-05-29 14:01:42 +0430

    debugged

--- IF_ID.v
+++ IF_ID.v
@@ -1,6 +1,6 @@
 `timescale 1ns / 1ns
 
-module IF_ID (input clk, rst, ifidWrite, ifidFlush, input [31:0] INS_out, new_PC, output [31:0] IFID_INS, IFID_PC);
+module IF_ID (input clk, rst, ifidWrite, ifidFlush, input [31:0] INS_out, new_PC, output reg [31:0] IFID_INS, IFID_PC);
   always @(posedge clk, posedge rst) begin
     if (rst || ifidFlush) begin
       IFID_INS <= 32'b0;
