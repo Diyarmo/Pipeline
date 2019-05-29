6f11ce237c3641fa7f1e2342a37c3195ab3a51bb
Diyar Mohammadi <diyar.mohammadi@ut.ac.ir>
2019-05-29 14:00:49 +0430

    debugged REGFILE module

--- REGFILE.v
+++ REGFILE.v
@@ -3,16 +3,15 @@
 module REGFILE (input clk, rst, input [4:0] ReadReg1, ReadReg2, WriteReg, input [31:0] writeData, input regWrite,
                 output reg [31:0] readData1, readData2);
   reg [31:0] mem [31:0];
-  initial begin
-    mem = 1024'b0;
-  end
+  integer i;
+  initial for (i=0; i < 32; i=i+1) mem[i] <= 32'b0;
   always @(negedge clk) begin
     readData1 <= mem[ReadReg1];
     readData2 <= mem[ReadReg2];
   end
   always @(posedge clk, posedge rst)
     if (rst)
-      mem = 1024'b0;
+      for (i=0; i < 32; i=i+1) mem[i] <= 32'b0;
     else if (regWrite) mem[WriteReg] <= writeData;
 
 endmodule
