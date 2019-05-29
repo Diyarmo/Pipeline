e624bff98ca301cbc8cecfd51430c34fb0769c93
Diyar Mohammadi <diyar.mohammadi@ut.ac.ir>
2019-05-29 14:04:10 +0430

    debugged

--- INSTRUCTION_MEMORY.v
+++ INSTRUCTION_MEMORY.v
@@ -1,9 +1,9 @@
 `timescale  1ns / 1ns
-module INSTRUCTION_MEMORY (input [31:0] PC_out, output [31:0] INS_out);
+module INSTRUCTION_MEMORY (input [31:0] PC_out, output reg [31:0] INS_out);
   reg [31:0] mem [10239:0];
-
+  integer i;
   initial begin
-    mem = 327680'b0;
+    for(i=0; i<10240; i=i+1) mem[i] <= 32'b0;
     // INSERT INSTRUCTIONS HERE for example mem[0] = 000000 00011 00010 00001 00000 100000 (without spaces)
     // is for add $1, $2, $3
   end
