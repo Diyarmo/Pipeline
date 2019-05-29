bec9e6841f14aa92ed6f7c2df0f1418b25d2fc52
Diyar Mohammadi <diyar.mohammadi@ut.ac.ir>
2019-05-29 14:05:00 +0430

    debugged

--- MEM_WB.v
+++ MEM_WB.v
@@ -1,7 +1,7 @@
 `timescale 1ns/1ns
 
 module MEM_WB (input clk, rst, input [1:0] WB, input [31:0] DM_out, ALU_out, input [4:0] writeReg,
-               input [1:0] WB_WB, input [31:0] WB_DM_out, WB_ALU_out, input [4:0] WB_writeReg);
+               output reg [1:0] WB_WB, output reg [31:0] WB_DM_out, WB_ALU_out, output reg [4:0] WB_writeReg);
 
   always @(posedge clk, posedge rst) begin
    if (rst) begin
