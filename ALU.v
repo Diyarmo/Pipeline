96a2af0554d36873e1567d245e6c0f0c2e910c31
Diyar Mohammadi <diyar.mohammadi@ut.ac.ir>
2019-05-29 16:06:38 +0430

    debugged

--- ALU.v
+++ ALU.v
@@ -1,12 +1,14 @@
 `timescale 1ns/1ns
 
-module ALU (input [31:0] A, B, input [2:0] ALUop, output [31:0] OUT);
+module ALU (input [31:0] A, B, input [2:0] ALUop, output reg [31:0] OUT);
   parameter ADD = 0, SUB = 1, AND = 2, OR = 3, SLT = 4;
-  case(ALUop):
-    ADD: assign OUT = A + B;
-    SUB: assign OUT = A - B;
-    AND: assign OUT = A & B;
-    OR : assign OUT = A | B;
-    SLT: assign OUT = (A == B);
-
+  always @(A, B, ALUop) begin
+    case(ALUop)
+      ADD: OUT = A + B;
+      SUB: OUT = A - B;
+      AND: OUT = A & B;
+      OR : OUT = A | B;
+      SLT: OUT = (A == B);
+    endcase
+  end
 endmodule
