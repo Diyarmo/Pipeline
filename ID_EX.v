9eead924af98818e5aa8a6e5536ed16cf8fa9234
Diyar Mohammadi <diyar.mohammadi@ut.ac.ir>
2019-05-29 13:56:41 +0430

    debugged ID_EX module

--- ID_EX.v
+++ ID_EX.v
@@ -1,8 +1,8 @@
 `timescale 1ns/1ns
 
 module ID_EX (input clk, rst, input [1:0] WB, M, input [4:0] EX, input [31:0] readData1, readData2,
-              S_EXTEND_out_b, input [4:0] rs, rt, rd, output [1:0] EX_WB, EX_M, output [4:0] EX_EX,
-              output [31:0] EX_SRC_A, EX_SRC_B, EX_SE, output [4:0] EX_rs, EX_rt, EX_rd);
+              S_EXTEND_out_b, input [4:0] rs, rt, rd, output reg [1:0] EX_WB, EX_M, output reg [4:0] EX_EX,
+              output reg [31:0] EX_SRC_A, EX_SRC_B, EX_SE, output reg [4:0] EX_rs, EX_rt, EX_rd);
   always @(posedge clk, posedge rst) begin
     if (rst) begin
       {EX_WB, EX_M, EX_EX} <= 9'b0;
