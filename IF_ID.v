d4904f1973f79ea34e2e3c9d1e59c50787fc5fca
Diyar Mohammadi <diyar.mohammadi@ut.ac.ir>
2019-05-31 13:14:13 +0430

    changed names

--- IF_ID.v
+++ IF_ID.v
@@ -1,14 +1,14 @@
 `timescale 1ns / 1ns
 
-module IF_ID (input clk, rst, ifidWrite, ifidFlush, input [31:0] INS_out, new_PC, output reg [31:0] IFID_INS, IFID_PC);
+module IF_ID (input clk, rst, ifidWrite, ifidFlush, input [31:0] INS_out, new_PC, output reg [31:0] ID_INS, ID_PC);
   always @(posedge clk, posedge rst) begin
     if (rst || ifidFlush) begin
-      IFID_INS <= 32'b0;
-      IFID_PC <= 32'b0;
+      ID_INS <= 32'b0;
+      ID_PC <= 32'b0;
     end
     else if (ifidWrite) begin
-      IFID_INS <= INS_out;
-      IFID_PC <= new_PC;
+      ID_INS <= INS_out;
+      ID_PC <= new_PC;
     end
   end
 
